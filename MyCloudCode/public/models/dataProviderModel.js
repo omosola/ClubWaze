8aec565f08ad5eb109135b5f9539e6b67212fd05
drokia2 <drokia2@gmail.com>
2013-11-02 14:00:55 -0700

    added skeleton form dataProviderModel.js

---
+++ MyCloudCode/public/models/dataProviderModel.js
@@ -0,0 +1,23 @@
+(function () {
+  "use strict";
+  BarHop.Models.DataProviderModel = Backbone.Model.extend({
+
+    getBarsNearGeolocation: function (attrs) {
+
+    }
+
+    getBarsInCity: function (attrs) {
+
+    }
+
+    saveUserInput: function (attrs) {
+
+    }
+
+    getBarObjectForId: function (attrs) {
+
+    }
+
+  });
+}());
+
