a1599723b708d4323a6aa16b43447f478ce65346
drokia2 <drokia2@gmail.com>
2013-11-03 10:57:35 -0800

    background color

--- MyCloudCode/public/js/views/bar_cell_view.js
+++ MyCloudCode/public/js/views/bar_cell_view.js
@@ -13,7 +13,7 @@
     },
 
 
-    template: _.template("<a href='barinfo.html?id=<%=id%>'><div class='bar-info'> <div> <%=name%> </div> <div> <%=dist%> </div> </div></a>"),
+    template: _.template("<a href='barinfo.html?id=<%=id%>'><div class='bar-info' style='background-color:<%=color%>'> <div> <%=name%> </div> <div> <%=dist%> </div> </div></a>"),
 
 
     initialize: function(){
@@ -23,7 +23,14 @@
 
     render: function() {
       console.log(this);
-      return this.template({id: this.options.id, name: this.options.name, dist: this.options.dist});
+      var colors = ['rgba(65,100,255,0.5)', 'rgba(255,69,0,0.5)'];
+      var rating = this.options.rating;
+      if (typeof(rating) == "undefined") {
+        rating = 2; 
+      }
+      var colorNum = rating*10 %2 ;
+      console.log(colorNum);
+      return this.template({id: this.options.id, name: this.options.name, dist: this.options.dist, color:colors[colorNum]});
     },
 
 
