faeee85801da59c1cce7a57d77c63538233433a6
davidzhuang <davidzhuang@server.fake>
2013-11-02 18:50:56 -0700

    update parse access function

--- MyCloudCode/public/js/models/data_provider_model.js
+++ MyCloudCode/public/js/models/data_provider_model.js
@@ -46,13 +46,36 @@
 
     },
 
-    saveUserInput: function (attrs) {
-      // addUserInput
+    saveRating: function (attrs) {
+      var Rating = Parse.Object.extend("Rating");
+      var rating = new Rating();
+
+      var jsonRating = JSON.stringify(attrs);
+      //rating.set("barId", attrs.barId);
+      //if (attrs.genderRatio != null)
+      //    rating.set("genderRatio", attrs.genderRatio);
+      //if (attrs.coverCharge != null)
+      //    rating.set("coverCharge", attrs.coverCharge);
+      //if (attrs.attractiveness != null)
+      //    rating.set("attractiveness", attrs.attractiveness);
+      //if (attrs.peopleDancing != null)
+      //    rating.set("peopleDancing", attrs.peopleDancing);
+      //if (attrs.atmosphere != null)
+      //    rating.set("atmosphere", attrs.atmosphere);
+      //if (attrs.busyness != null)
+      //    rating.set("busyness", attrs.busyness);
+      //if (attrs.entryLine != null)
+      //    rating.set("entryLine", attrs.entryLine);
+
+      if (jsonRating != null) {
+        rating.save(jsonRating);
+      }
     },
 
     getBarObjectForId: function (attrs) {
-       
-
+      var barWithRating = {};
+      barWithRating.bar = null;
+      barWithRating.rating = null;
 
     },
 
